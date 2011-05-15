a2d24af11b3323e2c180db81abab4ddb82d324df
uiii <uiii.dev@gmail.com>
2011-05-15 18:22:06 +0200

    Command code reorganisation
    - separating platform dependend code into files

---
+++ src/command_unix.cpp
@@ -0,0 +1,14 @@
+#include "command.h"
+
+#include <stdlib.h>
+#include <iostream>
+
+namespace FakeInput
+{
+    void Command::run(const std::string& cmd)
+    {
+        std::string command = cmd + " &";
+
+        system(command.c_str());
+    }
+}
--- src/command_win.cpp
+++ src/command_win.cpp
@@ -7,11 +7,7 @@ namespace FakeInput
 {
     void Command::run(const std::string& cmd)
     {
-#ifdef UNIX
-        std::string command = cmd + " &";
-#elif WIN32
         std::string command = "start " + cmd;
-#endif
 
         system(command.c_str());
     }
