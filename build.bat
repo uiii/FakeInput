b0e501020d1aea8588618bebbcc29c246168a12c
uiii <uiii.dev@gmail.com>
2011-06-08 19:20:15 +0200

    update building on windows

--- build.bat
+++ build.bat
@@ -1,17 +1,36 @@
 @echo off
 cd %~dp0
 
-if "%1"=="/r" (
-    if exist build (rmdir /S /Q build)
-    if exist bin (rmdir /S /Q bin)
-    if exist doc (rmdir /S /Q doc)
+set buildType=Release
+set test=OFF
+set doc=OFF
+
+for %%A in (%*) do (
+    if "%%A"=="/r" (
+        if exist build (rmdir /S /Q build)
+        if exist bin (rmdir /S /Q bin)
+        if exist doc (rmdir /S /Q doc)
+    )
+        
+    if "%%A"=="/D" ( :: debug build type
+        set buildType=Debug
+    )
+
+    if "%%A"=="/test" ( :: build testing applications
+        set test=ON
+    )
+
+    if "%%A"=="/doc" ( :: generate documentation
+        set doc=ON
+    )
 )
 
 if not exist build (mkdir build)
 cd build
 
 cls
-cmake ../ -DTEST_APP=ON -DCMAKE_BUILD_TYPE=Debug -DDOC=ON && msbuild FakeInput.sln /p:Configuration=Release
+    cmake ../ -DTEST_APP=%test% -DCMAKE_BUILD_TYPE=%buildType% -DDOC=%doc% && msbuild FakeInput.sln
+        :: /p:Configuration=Release
 
 cd ..
 
